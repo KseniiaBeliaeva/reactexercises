12f7029d083db99608e75c94ca369d098a27da0d
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-04-18 16:22:50 +1000

    Updating notes

--- Ch03/03_02/start/bullet-board/src/Note.js
+++ Ch03/03_02/start/bullet-board/src/Note.js
@@ -26,14 +26,18 @@ class Note extends Component {
     remove() {
         alert('Removing note')
     }
-    save() { 
-        alert(this._newText.value);
+    save(e) { 
+        e.preventDefault();
+        this.props.onChange(this._newText.value, this.props.index)
+        this.setState({
+            editing:false
+        })
     }
-
+  
     renderForm() { 
         return (
             <div className="note">
-                <form>
+                <form onSubmit={this.save}>
                     <textarea ref={input => this._newText = input}/>
                     <button onClick={this.save}>
                         <FaFloppyO />
